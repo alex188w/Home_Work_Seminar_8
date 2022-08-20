aeb786829f11d72424effff28c1408a06e4b0afd
Алексей Шевченко <alex18w@mail.ru>
2022-08-20 11:26:52 +0500

    Решение Семинар 8

---
+++ Zadacha_54_56/Program.cs
@@ -0,0 +1,95 @@
+﻿void Zadacha54()
+// Задайте двумерный массив. Напишите программу, которая упорядочит 
+// по убыванию элементы каждой строки двумерного массива
+{
+    Random random = new Random();
+    int rows = random.Next(4, 8);
+    int columns = random.Next(4, 8);
+    int[,] array = new int[rows, columns];
+    FillArray(array, 0, 99);
+    Console.WriteLine("Заданный массив ");
+    PrintArray(array);
+
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            int maxPosition = j;
+            for (int k = j + 1; k < columns; k++)
+            {
+                if(array[i, k] > array[i, maxPosition]) maxPosition = k;
+            }
+
+            int temporary = array[i, j];
+            array[i, j] = array[i, maxPosition];
+            array[i, maxPosition] = temporary;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine("Полученный массив ");
+    PrintArray(array);
+}
+
+void Zadacha56()
+// Задайте прямоугольный двумерный массив. Напишите программу,
+// которая будет находить строку с наименьшей суммой элементов
+{
+    Random random = new Random();
+    int rows = random.Next(4, 5);
+    int columns = rows;
+    int[,] array = new int[rows, columns];
+    FillArray(array, 0, 9);
+    Console.WriteLine("Заданный массив ");
+    PrintArray(array);
+
+    int minPosition = 0;    
+    int sum = 1000; // не знаю, как задать +бесконечность, по аналогии с нулем
+    
+    for (int i = 0; i < rows; i++) 
+    {       
+        int sum_i = 0;
+        for (int j = 0; j < columns; j++)
+        {            
+            sum_i = sum_i + array[i, j];       
+        }
+        if (sum_i < sum) 
+        {
+            minPosition = i;
+            sum = sum_i;
+        }
+        Console.WriteLine($"Сумма элементов строки {i} равна {sum_i}");
+    }
+    Console.WriteLine("Строка с наименьшей суммой элементов имеет номер " + minPosition);
+}
+
+void FillArray(int[,] array, int startNumber, int finishNumder)
+{
+    Random random = new Random();
+    finishNumder++;
+    int rows =  array.GetLength(0);
+    int colums = array.GetLength(1);
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            array[i, j] = new Random().Next(startNumber, finishNumder);   
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    int rows =  array.GetLength(0);
+    int colums = array.GetLength(1);
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < colums; j++)
+        {
+              Console.Write(array[i, j] + " \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+//Zadacha54();
+Zadacha56();
---
+++ Zadacha_58/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 58: Заполните спирально массив 4 на 4 числами от 1 до 16
+
+int rows = 4;
+int columns = 4;
+int[,] array = new int[rows, columns];
+int N = 1;
+array[0, 0] = N;
+FillArray(0, 1);
+PrintArray(array);
+
+void FillArray(int rows, int columns)
+{
+        if (array[rows, columns] == 0)
+        {          
+            N = N+1;
+            array[rows, columns] = N;
+            if (columns < (array.GetLength(1) - 1)) FillArray(rows, columns+1);
+            if (rows < (array.GetLength(0) - 1)) FillArray(rows+1, columns);
+            if (columns > 0) FillArray(rows, columns-1);
+            if (rows > 0) FillArray(rows-1, columns);
+        }        
+}
+
+void PrintArray(int[,] array)
+{
+    int rows =  array.GetLength(0);
+    int columns = array.GetLength(1);
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i, j] + " \t");
+        }
+        Console.WriteLine();
+    }
+}
